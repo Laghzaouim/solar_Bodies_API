939fadef68d4f93228f2b2e58bca878e24dc9c14
Mohammed La <mohammed.laghzaoui@outlook.com>
2018-05-16 12:00:01 +0200

    added planet property

---
+++ SolarBodies/Controllers/Model/Planet.cs
@@ -0,0 +1,13 @@
+namespace Model
+{
+    public class Planet
+    {
+        public int Id {get; set;}
+        public int Mass { get; set; }
+        public int Diameter { get; set; }
+        public int Density { get; set; }
+        public int LengthOfDay { get; set; }
+        public int DistanceFromSun { get; set; }
+        
+    }
+}
